0aaab10d309fb7a28a84c064f518a446dbe0eeca
Yozen Hernandez <yzhernand@gmail.com>
2017-05-25 15:43:16 -0400

    Corrected vars in batch script (no longer positional, now named)

--- scripts/qsub_test_advanced.sh
+++ scripts/qsub_test_advanced.sh
@@ -20,11 +20,11 @@ runnext=$?          # assuming die will always return 255 here and nothing in th
 case  $runnext  in
   0|1|4|10|13|15) 				# multiple
     echo "runnext state ($runnext), running in MULTI mode!"
-    qsub -cwd -l mem_total=64G -l h_rt=40:00:00 -pe omp $2 -v ARG1=$1,ARG2=$2,ARG3=$WORKD $scriptname
+    qsub -cwd -l mem_total=64G -l h_rt=40:00:00 -pe omp $nprocs -v ARG1=$dbsuffix,ARG2=$nprocs,ARG3=$WORKD $scriptname
     ;;
   2|3|5|6|7|8|9|11|12|14|16|17|18|19)      	# single
     echo "runnext state ($runnext), running in SINGLE mode!"
-    qsub -cwd -l h_rt=40:00:00 -v ARG1=$1,ARG2=$2,ARG3=$WORKD $scriptname
+    qsub -cwd -l h_rt=40:00:00 -v ARG1=$dbsuffix,ARG2=$nprocs,ARG3=$WORKD $scriptname
     ;;
   20)
     echo "No more qsub jobs needed ($runnext), done!"
