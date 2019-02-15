ecfc1bf6153a3b29a87e940c8414910e11c0520f
Yozen Hernandez <yzhernand@gmail.com>
2019-02-14 20:30:29 -0500

    Applied suggestions from shellcheck in sim_sw_reads.sh

--- scripts/sim_sw_reads.sh
+++ scripts/sim_sw_reads.sh
@@ -9,7 +9,7 @@ READ_LENGTH="$3"
 FLANK_LENGTH="$4"
 BUFFER=5 # buffer (in bases) around start/end positions
 
-if [ $READ_LENGTH -lt $(($FLANK_LENGTH + $BUFFER)) ]
+if [ "$READ_LENGTH" -lt $((FLANK_LENGTH + BUFFER)) ]
 then
 	echo "Read length must be longer than the flank length + $BUFFER";
 	exit;
@@ -27,7 +27,7 @@ TMPFILE=$(mktemp)
 # - one where the read ends at the TR start and goes until the read ends at TR+flank
 # - one where the read starts at the TR start-flank and goes until the read starts at TR end
 # Start position is just $2-(R+B)-1 since bedtools start pos is 0-based
-awk -F "," -v R=$READ_LENGTH -v F=$FLANK_LENGTH -v B=BUFFER '(NR>1){
+awk -F "," -v R="$READ_LENGTH" -v F="$FLANK_LENGTH" -v B=BUFFER '(NR>1){
 	if (($3-$2)+1+2*(F+B) < R ) {
 		print $5 "\t" ($2-(R+B)-1) "\t" ($2+B) "\t" $1;
 		# Break up window into two for smaller TRs
@@ -38,8 +38,8 @@ awk -F "," -v R=$READ_LENGTH -v F=$FLANK_LENGTH -v B=BUFFER '(NR>1){
 	}
 }' "$SEQ_FILE" > "$TMPFILE"
 
-bedtools getfasta -fi $REFERENCE -bed "$TMPFILE" | awk 'NR%2==0{ print}' | paste "$TMPFILE" - | \
-awk -v R=$READ_LENGTH -v F=$FLANK_LENGTH '{
+bedtools getfasta -fi "$REFERENCE" -bed "$TMPFILE" | awk 'NR%2==0{ print}' | paste "$TMPFILE" - | \
+awk -v R="$READ_LENGTH" -v F="$FLANK_LENGTH" '{
 	if(length($5)>=R){
 		for (i=0; i<=length($5)-R; i++) {
 			print ">" $1 ":" ($2+i) "-" ($2+i+R) "_" $4 "\n" substr($5,i,R)
