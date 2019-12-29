14af39f08f78eea0653c41065526ab064474c642
Yozen Hernandez <yzhernand@gmail.com>
2019-12-29 13:09:14 -0500

    Clean up of shell scripts based on shellcheck

--- scripts/master_for_qstub_test_advanced.sh
+++ scripts/master_for_qstub_test_advanced.sh
@@ -25,34 +25,24 @@ then
 fi
 
 batchname=${WORKD}/qsub_test_advanced.sh
-scriptname=${WORKD}/master_for_qstub_test_advanced.sh
 perlfile=${WORKD}/vntrseek.pl
 
-runnext=$(perl $perlfile 99 --dbsuffix $DBSUFFIX) # ask for what step needs to be run next
+runnext=$(perl "$perlfile" 99 --dbsuffix "$DBSUFFIX") # ask for what step needs to be run next
 
-STEPS=""
+STEPS=()
 
 handle_error() {
   runnext=$(perl "$perlfile" 99 --dbsuffix "$DBSUFFIX")
-  laststep=$(($runnext - 1))
+  laststep=$(("$runnext" - 1))
   echo "Oh noes, something went wrong (got SIGERR)! Setting run to status 2 (error)"
   echo "Job encountered error at step $laststep"
   exit 1
 }
 
-handle_kill() {
-  # A kill signal was sent.
-  runnext=$(perl "$perlfile" 99 --dbsuffix "$DBSUFFIX")
-  laststep=$(($runnext - 1))
-  echo "Oh noes, something went wrong (got SIGKILL)! Setting run to status 2 (error)"
-  echo "Job encountered error at step $laststep"
-  exit 1
-}
-
 handle_success() {
-  STATUS=""
+  # STATUS=""
   runnext=$(perl "$perlfile" 99 --dbsuffix "$DBSUFFIX")
-  laststep=$(($runnext - 1))
+  laststep=$((runnext - 1))
   if [ $laststep -eq 19 ]; then
     echo "Last step!"
     exit
@@ -61,51 +51,49 @@ handle_success() {
 }
 
 trap handle_error ERR
-trap handle_kill KILL
 
 if [ "$runnext" -gt 19 ]; then
   echo "Nothing to do, done!"
   exit 0
 elif [ "$runnext" -gt 17 ]; then
   echo "Doing steps 18 and 19!"       # single
-  STEPS="18 19"
+  STEPS=(18 19)
 elif [ "$runnext" -gt 15 ]; then
   echo "Doing steps 16 and 17!"       # single
-  STEPS="16 17"
+  STEPS=(16 17)
 elif [ "$runnext" -gt 14 ]; then
   echo "Doing step 15!"               # multiple
-  STEPS="15"
+  STEPS=(15)
 elif [ "$runnext" -gt 13 ]; then
   echo "Doing step 14!"               # single
-  STEPS="14"
+  STEPS=(14)
 elif [ "$runnext" -gt 12 ]; then
   echo "Doing step 13!"               # multiple
-  STEPS="13"
+  STEPS=(13)
 elif [ "$runnext" -gt 10 ]; then
   echo "Doing steps 11-12!"           # single
-  STEPS="11 12"
+  STEPS=(11 12)
 elif [ "$runnext" -gt 9 ]; then
   echo "Doing step 10!"               # multiple
-  STEPS="10"
+  STEPS=(10)
 elif [ "$runnext" -gt 4 ]; then
   echo "Doing steps 5-9!"             # single
-  STEPS="5 9"
+  STEPS=(5 9)
 elif [ "$runnext" -gt 3 ]; then
   echo "Doing step 4!"                # multiple
-  STEPS="4"
+  STEPS=(4)
 elif [ "$runnext" -gt 1 ]; then
   echo "Doing steps 2-3!"             # single
-  STEPS="2 3"
+  STEPS=(2 3)
 else
   echo "Doing steps 0-1!"             # multiple
-  STEPS="0 1"
+  STEPS=(0 1)
 fi
 
-if [ "$STEPS" ]; then
-  # Can't use "" around $STEPS; not seen as two space-separated args
-  perl "${perlfile}" ${STEPS} --dbsuffix "${DBSUFFIX}" --nprocesses ${NPROCS} &
+if [ "${STEPS[@]}" ]; then
+  perl "${perlfile}" "${STEPS[@]}" --dbsuffix "$DBSUFFIX" --nprocesses "$NPROCS" &
   wait "$!"
-  if [ $? -ne 0 ]; then
+  if ! wait "$!"; then
     handle_error "$DBSUFFIX"
   fi
   handle_success "$DBSUFFIX"
--- scripts/qsub_test_advanced.sh
+++ scripts/qsub_test_advanced.sh
@@ -7,22 +7,22 @@ then
 fi
 
 WORKD="$( dirname "${BASH_SOURCE[0]}" )"
-scriptname=${WORKD}/master_for_qstub_test_advanced.sh
-perlfile=${WORKD}/vntrseek.pl
+scriptname="$WORKD/master_for_qstub_test_advanced.sh"
+perlfile="$WORKD/vntrseek.pl"
 
 nprocs=$1
 dbsuffix="$2"
 
-runnext=$(perl $perlfile 99 --dbsuffix $dbsuffix)   # ask for what step needs to be run next
+runnext=$(perl "$perlfile" 99 --dbsuffix "$dbsuffix")   # ask for what step needs to be run next
 
 case  $runnext  in
   0|1|4|10|13|15) 				# multiple
     echo "runnext state ($runnext), running in MULTI mode!"
-    qsub -cwd -N vntrseek_"$dbsuffix"_step"$runnext" -l mem_total=64G -l h_rt=40:00:00 -pe omp $nprocs -v DBSUFFIX=$dbsuffix,NPROCS=$nprocs,WORKD=$WORKD,runnext=$runnext $scriptname
+    qsub -cwd -N vntrseek_"$dbsuffix"_step"$runnext" -l mem_total=64G -l h_rt=20:00:00 -pe omp "$nprocs" -v DBSUFFIX="$dbsuffix",NPROCS="$nprocs",WORKD="$WORKD",runnext="$runnext" "$scriptname"
     ;;
   2|3|5|6|7|8|9|11|12|14|16|17|18|19)      	# single
     echo "runnext state ($runnext), running in SINGLE mode!"
-    qsub -cwd -N vntrseek_"$dbsuffix"_step"$runnext" -l h_rt=40:00:00 -v DBSUFFIX=$dbsuffix,NPROCS=$nprocs,WORKD=$WORKD,runnext=$runnext $scriptname
+    qsub -cwd -N "vntrseek_${dbsuffix}_step$runnext" -l h_rt=20:00:00 -v DBSUFFIX="$dbsuffix",NPROCS="$nprocs",WORKD="$WORKD",runnext="$runnext" "$scriptname"
     ;;
   20)
     echo "No more qsub jobs needed ($runnext), done!"
